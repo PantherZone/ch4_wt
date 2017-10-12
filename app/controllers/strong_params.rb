e14e152e9e835d704d41e50386590242df893a73
Blake M <bmarti66@uncc.edu>
2017-10-11 20:49:52 -0400

    Add files via upload

---
+++ app/controllers/strong_params.rb
@@ -0,0 +1,8 @@
+class MoviesController < ApplicationController
+  def create
+    params.require(:movie)
+    params[:movie].permit(:title,:rating,:release_date)
+    # shortcut: params.require(:movie).permit(:title,:rating,:release_date)
+    # rest of code...
+  end
+end
